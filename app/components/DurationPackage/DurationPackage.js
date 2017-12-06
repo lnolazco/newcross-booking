5d30fa43f151a033d3c255a95320f64ef26ec6c0
Luis Nolazco <lnolazco@gmail.com>
2017-12-06 21:02:41 +0000

    add action to duration package

--- app/components/DurationPackage/DurationPackage.js
+++ app/components/DurationPackage/DurationPackage.js
@@ -1,11 +1,34 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { Card, Row, Col, DatePicker, Radio } from 'antd';
 
 const RadioGroup = Radio.Group;
 
 class DurationPackage extends PureComponent {
+  state = {
+    startDate: null,
+    endDateSpecific: false,
+    endDate: null,
+  }
+
   onChangeStartDate = (value) => {
-    this.setState({ startDate: value });
+    this.setState({ startDate: value }, () => {
+      this.props.onChange(this.state);
+    });
+  }
+
+  onChangeEndDate = (value) => {
+    this.setState({ endDate: value }, () => {
+      this.props.onChange(this.state);
+    });
+  }
+
+  onChangeEndDateRadio = (e) => {
+    this.setState({
+      endDateSpecific: e.target.value === 'specific',
+    }, () => {
+      this.props.onChange(this.state);
+    });
   }
 
   render() {
@@ -32,15 +55,28 @@ class DurationPackage extends PureComponent {
           </Col>
           <Col span={12}>
             <h4>End Date</h4>
-            <RadioGroup onChange={this.onChange} >
-              <Radio style={radioStyle} value={1}>Ongoing</Radio>
-              <Radio style={radioStyle} value={2}>Specific</Radio>
+            <RadioGroup onChange={this.onChangeEndDateRadio} >
+              <Radio style={radioStyle} value="ongoing">Ongoing</Radio>
+              <Radio style={radioStyle} value="specific">Specific</Radio>
             </RadioGroup>
+            {
+              this.state.endDateSpecific && (
+                <DatePicker
+                  format="DD/MM/YYYY"
+                  placeholder="dd/mm/yyyy"
+                  onChange={this.onChangeEndDate}
+                />
+              )
+            }
           </Col>
         </Row>
       </Card>
     );
   }
 }
+
+DurationPackage.propTypes = {
+  onChange: PropTypes.func.isRequired,
+};
 
 export default DurationPackage;
